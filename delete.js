1e4620e863d5722a95154781a33864fb1ed1e38f
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-10-26 08:51:05 -0500

    promise

---
+++ delete.js
@@ -0,0 +1,24 @@
+
+const myFunction = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('paso 2');
+        }, 2000);
+    })
+}
+
+//console.log(myFunction()); //
+console.log('paso 1');
+myFunction()
+    .then((result) => {
+        console.log(result);
+        return `${result} y luego paso 4`;
+    })
+    .then((resultTwo)=>{
+        console.log('linea 18', resultTwo);
+    })
+    .catch((error) => {
+
+    });
+
+console.log('paso 3');
--- http.js
+++ http.js
@@ -1,21 +1,27 @@
 import fetch from 'node-fetch';
 
 export const validateLinks = (link) => {
-  
+
     return fetch(link)
-        // .then((response) => {
+        .then((response) => {
 
-        //     let status = response.status
-        //     let message = response.statusText
-        //     console.log(status, message);
+            let status = response.status
+            let message = response.statusText
+            return { status, message }
 
-        // }).catch((error) => {
+        }).catch((error) => {
 
-        //     let status = error.message
-        //     let message = 'fail'
-        //     console.log(status, message);
-        // });
-
+            let status = error.message
+            let message = 'fail'
+            return { status, message }
+        });
 }
 
-// console.log(validateLinks('https://github.com/Dsooadha/CDMX013-md-links/blob/main/pruebasMD/README.md'))
+
+
+validateLinks('https://google.com')
+    .then((res) => {
+        console.log({ status, message });
+    })
+
+
